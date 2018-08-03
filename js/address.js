ed26ed3410f746ed7bf9fa3be37f238c7c1e5352
Ira Miller <public@iramiller.com>
2018-08-03 12:00:34 -0500

    color time warnings

--- js/address.js
+++ js/address.js
@@ -1,3 +1,8 @@
+// defaults
+window.quote = 'USD'
+window.pay = 'BTC'
+window.base = 'GULD'
+
 // from https://stackoverflow.com/a/21739514
 function roundTimeQuarterHour(time) {
     time = time || new Date()
@@ -8,9 +13,60 @@ function roundTimeQuarterHour(time) {
     return timeToReturn
 }
 
+function parseCommodityPrice (pricef, commodity = 'GULD', quote = 'USD') {
+  var pricefl
+  var pricea
+  var amtstr
+  var re
+  commodity = commodity.toUpperCase()
+  quote = quote.toUpperCase()
+  pricef = pricef.split('\n').reverse()
+  pricefl = pricef.filter(filterPricesByTime)
+  var res = `[0-9.]*[ ]{0,1}${quote}$`.replace(commodity, '')
+  re = new RegExp(res, 'm')
+  pricea = re.exec(pricefl.join('\n'))
+  if (pricea && pricea.length > 0 && pricea[0].length > 0) {
+    amtstr = pricea[0].replace(commodity, '').trim()
+    var amt = amtstr.replace(quote, '').trim()
+    return amt
+  } else throw new RangeError(`Price not found for commodity ${commodity}`)
+}
+
+function updatePrice (quote, base, market='coinmarketcap') {
+  quote = quote || window.quote
+  base = base || window.base
+  console.log($)
+  console.log($.ajax)
+  console.log($.Ajax)
+  console.log(quote)
+  console.log(base)
+  if (base === 'GULD') market = 'guld-core'
+  function process (data) {
+    console.log(data)
+    var activePrices = document.getElementsByClassName("active-price");
+
+    for(var i = activePrices.length - 1; i >= 0; i--) {
+        timestampArray[i].innerHTML = parseCommodityPrice(data, base, quote)
+    }
+
+  }
+  $.ajax({
+    url: `/market/${quote}/${base}/prices/${market}.dat`,
+  })
+  .done(process)
+  .success(process)
+  .fail(function() {
+    alert("Ajax failed to fetch data")
+  })
+}
+
 // COUNTDOWN
 // Set the date we're counting down to
 var countDownDate = roundTimeQuarterHour().getTime();
+
+$(document).ready(function () {
+    updatePrice()
+})
 
 // Update the count down every 1 second
 var x = setInterval(function () {
@@ -32,12 +88,13 @@ var x = setInterval(function () {
     }
 
     // Display clock
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
+    document.getElementById("minutes").innerText = minutes
+    document.getElementById("seconds").innerText = seconds
 
     var warning = `<h6 class="card-title ALERT_CLASS">Your GULD will be credited at the exchange rate given by <a href="https://coinmarketcap.com" target="_blank">coinmarketcap</a> at the start of the 15 minute window when the transaction is first witnessed by isysd, or when the first confirmation is received, whichever is first.</h6>`
     if (distance < 0) {
-        countDownDate = roundTimeQuarterHour().getTime();
+        countDownDate = roundTimeQuarterHour().getTime()
+        updatePrice()
     } else if (distance < 300000) {
         document.getElementById("time-warning").innerHTML = warning.replace('ALERT_CLASS', 'alert-danger')
     } else if (distance < 600000) {
