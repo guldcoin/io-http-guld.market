c11ce87579e38be992ed428fc1e369c3bc042e3b
Ira Miller <public@iramiller.com>
2018-08-03 14:08:39 -0500

    load assets

--- js/address.js
+++ js/address.js
@@ -2,6 +2,26 @@
 window.quote = 'USD'
 window.pay = 'BTC'
 window.base = 'GULD'
+window.bases = {
+  'GULD': {
+    'image': '/img/guld.svg'
+  },
+  'ISYSD': {
+    'image': '/img/wizard-head.png'
+  }
+}
+
+window.quotes = {
+  'BTC': {
+    'image': '/img/btc.svg'
+  },
+  'DASH': {
+    'image': '/img/dash.png'
+  },
+  'ETH': {
+    'image': '/img/eth.png'
+  }
+}
 
 // from https://stackoverflow.com/a/21739514
 function roundTimeQuarterHour(time) {
@@ -49,25 +69,45 @@ function parseCommodityPrice (pricef, commodity = 'GULD', quote = 'USD') {
 function updatePrice (quote, base, market='coinmarketcap') {
   quote = quote || window.quote
   base = base || window.base
-  console.log(quote)
-  console.log(base)
   if (base === 'GULD') market = 'guld-core'
   function process (data) {
-    console.log(data)
     var activePrices = document.getElementsByClassName("active-price");
-
     for(var i = activePrices.length - 1; i >= 0; i--) {
         activePrices[i].innerText = parseCommodityPrice(data, base, quote)
     }
-
   }
   $.ajax({
     url: `/market/${quote}/${base}/prices/${market}.dat`,
   })
   .done(process)
   .fail(function() {
-    alert("Ajax failed to fetch data")
+    console.log("Ajax failed to fetch data")
   })
+}
+
+function loadAssetList () {
+  var qel = document.getElementById('quote-list')
+  qel.innerHTML = ''
+  var qlist = Object.keys(window.quotes)
+  for (var q = 0; q < qlist.length; q++) {
+    var quote = qlist[q]
+    qel.innerHTML = `${qel.innerHTML}
+<div class="card card-asset">
+  <img class="card-img-top" src="${window.quotes[quote].image}" alt="Deposit ${quote}" title="Deposit ${quote}">
+</div>
+`
+  }
+  var bel = document.getElementById('base-list')
+  bel.innerHTML = ''
+  var blist = Object.keys(window.bases)
+  for (var b = 0; b < blist.length; b++) {
+    var base = blist[b]
+    bel.innerHTML = `${bel.innerHTML}
+<div class="card card-asset">
+  <img class="card-img-top" src="${window.bases[base].image}" alt="Receive ${base}" title="Receive ${base}">
+</div>
+`
+  }
 }
 
 // COUNTDOWN
@@ -75,6 +115,7 @@ function updatePrice (quote, base, market='coinmarketcap') {
 var countDownDate = roundTimeQuarterHour().getTime();
 
 $(document).ready(function () {
+    loadAssetList()
     updatePrice()
 })
 
@@ -100,8 +141,7 @@ var x = setInterval(function () {
     // Display clock
     document.getElementById("minutes").innerText = minutes
     document.getElementById("seconds").innerText = seconds
-
-    var warning = `<h6 class="card-title ALERT_CLASS">Your GULD will be credited at the exchange rate given by <a href="https://coinmarketcap.com" target="_blank">coinmarketcap</a> at the start of the 15 minute window when the transaction is first witnessed by isysd, or when the first confirmation is received, whichever is first.</h6>`
+    var warning = `<h6 class="card-title ALERT_CLASS">Your GULD will be credited at the exchange rate given by <a href="https://coinmarketcap.com" target="_blank">coinmarketcap</a> at the start of the 15 minute window when the transaction is first witnessed. <a href="https://guld.legal/contract/template/trade/INTERNATIONAL_SALE_GOODS_U1000.html">Sales Agreement</a></h6>`
     if (distance < 0) {
         countDownDate = roundTimeQuarterHour().getTime()
         updatePrice()
