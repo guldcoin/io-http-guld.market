30cf9b29c93993a399ba7c1ad58e18493bcfe7ec
Ira Miller <public@iramiller.com>
2018-08-03 17:37:53 -0500

    mobile and url fixes

--- js/address.js
+++ js/address.js
@@ -230,7 +230,7 @@ var x = setInterval(function () {
     // Display clock
     document.getElementById("minutes").innerText = minutes
     document.getElementById("seconds").innerText = seconds
-    var warning = `<h6 class="card-title ALERT_CLASS">Your GULD will be credited at the exchange rate given by <a href="https://coinmarketcap.com" target="_blank">coinmarketcap</a> at the start of the 15 minute window when the transaction is first witnessed. <a href="https://guld.legal/contract/template/trade/INTERNATIONAL_SALE_GOODS_U1000.html">Sales Agreement</a></h6>`
+    var warning = `<h6 class="card-title ALERT_CLASS">Your <span class="base-asset">${window.bases.active}</span> will be credited at the exchange rate given by <a href="https://coinmarketcap.com" target="_blank">coinmarketcap</a> at the start of the 15 minute window when the transaction is first witnessed. <a href="https://guld.legal/contract/template/trade/INTERNATIONAL_SALE_GOODS_U1000.html">Sales Agreement</a></h6>`
     if (distance < 0) {
         countDownDate = roundTimeQuarterHour().getTime()
         updatePrice()
